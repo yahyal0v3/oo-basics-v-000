ed050482da5a39a6382b730567ad70be73dce47d
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-08 04:42:15 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand :condition
+  attr_reader :brand, :condition
 
   def initialize(brand)
     @brand = brand
